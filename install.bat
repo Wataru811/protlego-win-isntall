37f926fc3ba1bbc03d1a5195e891b834f18088fd
Wataru811 <wash.fuel@gmail.com>
2021-02-20 12:43:49 +0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,21 @@
+echo "python3, visual C がインストールされている前提です。
+start chrome https://www.python.org/downloads/
+start chrome https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community
+
+SET /P ANSWER="実行します。よろしいですか (Y/N)？"
+
+if /i {%ANSWER%}=={y} (goto :yes)
+if /i {%ANSWER%}=={yes} (goto :yes)
+
+EXIT
+
+:yes
+python --version
+pip --version
+python -m pip install --upgrade pip
+pip --version
+pip install protlego
+
+
+
+
